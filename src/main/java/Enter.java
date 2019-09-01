dcd3213fc4e1181fb97ddfd72020c633f354ea75
omudritsky <omudritsky@gmail.com>
2019-09-01 20:37:24 +0300

    Счелкает 2 вниз

--- src/main/java/Enter.java
+++ src/main/java/Enter.java
@@ -3,11 +3,10 @@ import com.google.common.annotations.VisibleForTesting;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Convert;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,6 +18,7 @@ public class Enter {
     public static void comeIn() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\chrome\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://clone.ewa.ua/#/home");
         Thread.sleep(3000);
         driver.findElement(By.xpath("//div[@id='email']/div/input")).sendKeys("45076@ewa.ua");
@@ -36,15 +36,16 @@ public class Enter {
     @Test
     public void makeOsago() throws InterruptedException {
         driver.findElement(By.xpath("//div/div/span")).click(); //нажатие на вападающий список продуктов осаго.туризм...
-        Thread.sleep(3000);
+        Thread.sleep(2000);
         WebElement product = driver.findElement(By.xpath("//div[@id='tariff']/div/div/div"));// вападающий список продуктов осаго
         product.click();
-        Thread.sleep(5000);
-       // product.sendKeys(Keys.ARROW_DOWN);
-        //product.sendKeys(Keys.ARROW_DOWN);
+        Thread.sleep(2000);
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.ARROW_DOWN).build().perform();
+        action.sendKeys(Keys.ARROW_DOWN).build().perform();
+        action.sendKeys(Keys.ENTER).build().perform();
 
-        product.sendKeys(Keys.ENTER);
-        product.sendKeys(Keys.ENTER);
+
     }
 
     @AfterClass

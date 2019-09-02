cba74821a53026e3ba38e78da6603967097e907c
omudritsky <omudritsky@gmail.com>
2019-09-02 21:56:53 +0300

    Osago Sdelano!! Polis vipuskaetsa

--- src/main/java/Enter.java
+++ src/main/java/Enter.java
@@ -36,14 +36,122 @@ public class Enter {
     @Test
     public void makeOsago() throws InterruptedException {
         driver.findElement(By.xpath("//div/div/span")).click(); //нажатие на вападающий список продуктов осаго.туризм...
-        Thread.sleep(2000);
+            Thread.sleep(2000);
+
         WebElement product = driver.findElement(By.xpath("//div[@id='tariff']/div/div/div"));// вападающий список продуктов осаго
-        product.click();
+            product.click();
+            Thread.sleep(2000);
+            Actions action = new Actions(driver);
+            action.sendKeys(Keys.ARROW_DOWN).build().perform();
+            action.sendKeys(Keys.ARROW_DOWN).build().perform();
+            action.sendKeys(Keys.ENTER).build().perform();
+            Thread.sleep(1000);
+
+        WebElement tiptrans = driver.findElement(By.xpath("//div[@id='autoCategory']/div/div/div"));// тип тз
+            tiptrans.click();
+            Actions tiptz = new Actions(driver);
+            tiptz.sendKeys(Keys.ARROW_DOWN).build().perform();
+            tiptz.sendKeys(Keys.ARROW_DOWN).build().perform();
+            tiptz.sendKeys(Keys.ENTER).build().perform();
+            Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//div[@id='registrationPlace']/div/div/div")).click();//misto reestratsii
+            Thread.sleep(1000);
+            Actions misto = new Actions(driver);
+            misto.sendKeys(Keys.ENTER).build().perform();
+            Thread.sleep(1000);
+
+
+        driver.findElement(By.xpath("//div[@id='registrationType']/div/div/div")).click();//Tip Reestratsii
+            Actions tipReestr = new Actions(driver);
+            tipReestr.sendKeys(Keys.ENTER).build().perform();
+
+//Dani straxyvalnika
+
+        WebElement inn = driver.findElement(By.xpath("//div[@id='customerCode']/div/div/input"));//INN
+            inn.sendKeys("3478763498");
+
+        driver.findElement(By.xpath("//div[@id='customerBirthDate']/div/div/input")).click(); //date rogdenia
+
+        WebElement familia = driver.findElement(By.xpath("//div[@id='customerNameLast']/div/input")); //familia
+        familia.sendKeys("Testov");
+
+        WebElement name = driver.findElement(By.xpath("//div[@id='customerNameFirst']/div/input")); //name
+        name.sendKeys("Test");
+
+        WebElement otchestvo = driver.findElement(By.xpath("//div[@id='customerNameMiddle']/div/input")); //otchestvo
+        otchestvo.sendKeys("Testovich");
+
+
+        WebElement adress = driver.findElement(By.xpath("//div[2]/div/div/div/div/div/div/div/div/div/div/div/input")); //adress
+        adress.sendKeys("Kiev Balzaka str 61");
+
+        WebElement tel = driver.findElement(By.xpath("//div[@id='customerPhone']/div/input")); //telefon
+        tel.sendKeys("098-662-48-68");
+
+        WebElement email = driver.findElement(By.xpath("//div[@id='customerEmail']/div/input")); //email
+        email.sendKeys("omudritsky@ingo.ua");
+
+
+        driver.findElement(By.xpath("//div[@id='customerDocumentType']/div/div/div")).click();//doc osobi
+        Thread.sleep(1000);
+        Actions doc = new Actions(driver);
+        doc.sendKeys(Keys.ENTER).build().perform();
+        Thread.sleep(1000);
+
+
+        WebElement seria = driver.findElement(By.xpath("//div[@id='customerDocumentSeries']/div/input")); //seria doc
+        seria.sendKeys("ПП");
+
+        WebElement nomer = driver.findElement(By.xpath("//div[@id='customerDocumentNumber']/div/input")); //nomer doc
+        nomer.sendKeys("123456");
+
+
+        WebElement dataVidachi = driver.findElement(By.xpath("//div[@id='customerDocumentDate']/div/div/input")); //date vidachi doc
+        Thread.sleep(1000);
+        dataVidachi.sendKeys("01.01.2010");
+
+
+        WebElement kimvidan = driver.findElement(By.xpath("//div[3]/div/div/div[3]/div/div/div/div/div/div/div/div/div/div/input")); //kim vudan doc
+        kimvidan.sendKeys("Деснянським РУ ГУ");
         Thread.sleep(2000);
-        Actions action = new Actions(driver);
-        action.sendKeys(Keys.ARROW_DOWN).build().perform();
-        action.sendKeys(Keys.ARROW_DOWN).build().perform();
-        action.sendKeys(Keys.ENTER).build().perform();
+        Actions vidan = new Actions(driver);
+        vidan.sendKeys(Keys.ARROW_DOWN).build().perform();
+        vidan.sendKeys(Keys.ENTER).build().perform();
+
+//Dani Avto
+
+        WebElement numberAvto = driver.findElement(By.xpath("//div[@id='stateNumber']/div/div/input"));  //numberAvto
+        numberAvto.sendKeys("AA0560TB");
+
+        WebElement vin = driver.findElement(By.xpath("//div[@id='bodyNumber']/div/div/input"));  //Vin
+        vin.sendKeys("WDF12376456789098");
+
+        WebElement model = driver.findElement(By.xpath("//div[@id='autoModel']/div/div/input"));  //model
+        model.sendKeys("308");
+        Thread.sleep(2000);
+        model.sendKeys(Keys.ARROW_DOWN);
+        model.sendKeys(Keys.ENTER);
+
+        WebElement year = driver.findElement(By.xpath("//div[@id='autoYear']/div/div/input"));  //year
+        year.sendKeys("2008");
+
+        WebElement engine = driver.findElement(By.xpath("//div[@id='engineVolume']/div/div/input")); //engine
+        engine.sendKeys("1555");
+
+        WebElement adressDostavki = driver.findElement(By.xpath("//div[7]/div/div/div/div/input")); //Адрес сдоставки
+        adressDostavki.sendKeys("Гонолулу, квартира 25");
+
+
+
+//Podpisat dogovor
+
+        driver.findElement(By.xpath("//div[@id='buttonSign2']/div/div")).click();   // Podpisat
+
+
+
+
+
 
 
     }
@@ -51,7 +159,7 @@ public class Enter {
     @AfterClass
     public static void end() throws InterruptedException {
         Thread.sleep(10000);
-        driver.close();
+       // driver.close();
 
     }
 
